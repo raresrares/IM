5659bcc98c17be5e2c8b8383d497a4b57a9c1511
Nazdra <alexanazdravan@gmail.com>
2022-05-20 15:50:07 +0300

    vibe check

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -4,6 +4,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
+//nazdra a pus comm
+
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
